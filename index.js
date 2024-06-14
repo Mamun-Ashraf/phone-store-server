d53af90ec6f2906076046b76b4580658c49329ae
Mamun-Ashraf <ashrafdkia@gmail.com>
2024-06-14 17:47:58 +0600

    added phone APIs

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 const express = require('express');
 const app = express()
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
@@ -86,6 +87,42 @@ async function run() {
                 { $set: userData },
                 { upsert: true }
             );
+            res.send(result);
+        });
+
+        // phones
+
+        app.get("/phones", async (req, res) => {
+            const result = await phoneCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.get("/phone/:id", async (req, res) => {
+            const id = req.params.id;
+            const phoneData = await phoneCollection.findOne({
+                _id: new ObjectId(id),
+            });
+            res.send(phoneData);
+        });
+
+        app.post("/phone", verifyToken, async (req, res) => {
+            const phoneData = req.body;
+            const result = await phoneCollection.insertOne(phoneData);
+            res.send(result);
+        });
+
+        app.patch("/phone/:id", verifyToken, async (req, res) => {
+            const id = req.params.id;
+            const updatedData = req.body;
+            const result = await phoneCollection.updateOne(
+                { _id: new ObjectId(id) },
+                { $set: updatedData }
+            );
+            res.send(result);
+        });
+        app.delete("/phone/:id", verifyToken, async (req, res) => {
+            const id = req.params.id;
+            const result = await phoneCollection.deleteOne({ _id: new ObjectId(id) });
             res.send(result);
         });
 
